94dbea258771e6ae08ad9f0aa46f5477122ba48f
Eli Joesph Bradley <elijos03@gmail.com>
2017-09-14 20:15:23 -0500

    Allow connections between servers.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { Election, irv } = require('caritat');
+const cors = require('cors');
 const md5 = require('blueimp-md5');
 const app = express();
 const url = require('url');
 
 app.set('port', (process.env.PORT || 5000));
 const masterKey = 'd95408eb72112b35ac5e208fdc1309f3';
+app.use(cors({ origin: process.env.PRODUCTION ? 'http://127.0.0.1:3000/' : 'https://react-vote.herokuapp.com/'}))
 
 global.votes = process.env.PRODUCTION ? {} : {
     president: {
@@ -36,8 +38,14 @@ global.votes = process.env.PRODUCTION ? {} : {
     }
 };
 
+global.locked = [];
+
 app.get('/vote', function (req, res) {
     console.log(req.query);
+
+    if (global.locked.includes(req.query.role)) {
+        res.send(`${req.query.role} voting is locked.`);
+    }
 
     global.votes = {
         ...global.votes,
@@ -73,19 +81,33 @@ app.get('/winner', function (req, res) {
         election.addBallot(votes[voter]);
     }
 
-    console.log(irv(election))
+    console.log(irv(election));
 
-    res.send(irv(election))
+    res.send(irv(election));
+});
+
+app.get('/locked', function (req, res) {
+    
+});
+
+app.get('/lock', function (req, res) {
+    if (md5(req.query.password) !== masterKey) {
+        res.status(401).end();
+    } else {
+        global.locked.push(req.query.role);
+
+        res.send(`Locked ${role}`);
+    }
 });
 
 app.get('/reset', function (req, res) {
     if (md5(req.query.password) !== masterKey) {
-        res.status(404).end();
+        res.status(401).end();
     } else {
         global.votes = {};
         res.send('Reset successful.');
     }
-})
+});
 
 // Debugging/spam-prevention method.
 app.get('/illuminacho_portal', function (req, res) {
@@ -100,3 +122,4 @@ app.get('/illuminacho_portal', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
+
